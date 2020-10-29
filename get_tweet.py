d8fd236476d84082b48311460936b423124e8edc
izanagi389 <izanagiokami389@gmail.com>
2020-10-29 16:59:23 +0900

    文字数を制限

--- get_tweet.py
+++ get_tweet.py
@@ -1,8 +1,8 @@
 import json
 from config import config
-import csv # csv読み取り
+import csv  # csv読み取り
 import requests
-from requests_oauthlib import OAuth1Session #OAuthのライブラリの読み込み
+from requests_oauthlib import OAuth1Session  # OAuthのライブラリの読み込み
 import os
 import pandas as pd
 import re
@@ -14,9 +14,10 @@ CS = config.CS
 AT = config.AT
 AS = config.AS
 
+
 def main():
-    twitter = OAuth1Session(CK, CS, AT, AS) #認証処理
-    url_base = "https://api.twitter.com/1.1/statuses/user_timeline.json?tweet_mode=extended&user_id="#タイムライン取得エンドポイント
+    twitter = OAuth1Session(CK, CS, AT, AS)  # 認証処理
+    url_base = "https://api.twitter.com/1.1/statuses/user_timeline.json?tweet_mode=extended&user_id="  # タイムライン取得エンドポイント
     user_id_list = config.USER_ID_LIST
 
     for id in user_id_list:
@@ -25,35 +26,38 @@ def main():
 
 def getTweet(id, url_base, twitter):
     url = url_base + id
-    params ={'include_rts' : False, 'exclude_replies' : True,'count' : 200, } #取得数,リプライを除く
-    res = twitter.get(url, params = params)
+    params = {'include_rts': False, 'exclude_replies': True, 'count': 200, }  # 取得数,リプライを除く
+    res = twitter.get(url, params=params)
 
-    if res.status_code == 200: #正常通信出来た場合
-        timelines = json.loads(res.text) #レスポンスからタイムラインリストを取得
+    if res.status_code == 200:  # 正常通信出来た場合
+        timelines = json.loads(res.text)  # レスポンスからタイムラインリストを取得
 
         count = len(timelines)
-        account_name = timelines[0]['user']['name'].replace('/', '') #アカウント名の取得
+        account_name = timelines[0]['user']['name'].replace('/', '')  # アカウント名の取得
         with open('data/csv/' + account_name + '.csv', 'w') as f:
             writer = csv.writer(f)
 
-            for line in timelines: #タイムラインリストをループ処理
+            for line in timelines:  # タイムラインリストをループ処理
                 # csv書き込み
                 text = "".join(line['full_text'].splitlines())
-                text = re.sub(r'[^ ]+\.[^ ]+','',text)
+                text = re.sub(r'[^ ]+\.[^ ]+', '', text)
                 # 絵文字を削除
                 text = ''.join(c for c in text if c not in emoji.UNICODE_EMOJI)
                 # 全角記号削除
-                text = re.sub(r'[︰-＠]','',text)
+                text = re.sub(r'[︰-＠]', '', text)
                 # 半角記号削除 + 半角数字
-                text = re.sub(r'[!-@[-`{-~]','',text)
-                writer.writerow([text, line['created_at']]) # result[0], result[1]
+                text = re.sub(r'[!-@[-`{-~]', '', text)
+                if not len(text) <= 20:
+                    writer.writerow([text, line['created_at']])  # result[0], result[1]
+                else:
+                    count -= 1
 
         print(account_name + "tweets get! " + str(count) + " piece")
-    else: #正常通信出来なかった場合
+    else:  # 正常通信出来なかった場合
         print("Failed: %d" % res.status_code)
 
-main()
 
+main()
 
 
 # gcp感情分析
